0ee1d3e4c4d87b0a54a93b96d1d364510144d95d
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-29 13:47:55 -0500

    login and register endpoint tests functioning

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,59 @@
+const request = require("supertest");
+const db = require("../data/dbConfig");
+const server = require("./server");
+
+const User = require("./auth/auth-model");
+
+const samUser = { username: "sam", password: "1234" };
+
+const incompleteUser = { username: "", password: "" };
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db("users").truncate();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+describe("server", () => {
+  describe("[POST] /register", () => {
+    it("Denies if credentials aren't supplied", async () => {
+      let res = await request(server)
+        .post("/api/auth/register")
+        .send(incompleteUser);
+      expect(res.status).toEqual(400);
+    });
+    it("sends user back on success", async () => {
+      let res = await await request(server)
+        .post("/api/auth/register")
+        .send(samUser);
+      expect(res.body).toMatchObject({ username: "sam" });
+    });
+  });
+
+  describe("[POST] /login", () => {
+    it("denies if user doesn't exist", async () => {
+      let res = await request(server).post("/api/auth/login").send(samUser);
+      expect(res.status).toEqual(401);
+    });
+    it("grants places token with string in body if login success", async () => {
+      await request(server).post("/api/auth/register").send(samUser);
+      let res = await request(server).post("/api/auth/login").send(samUser);
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          token: expect.any(String),
+        })
+      );
+    });
+  });
+});
