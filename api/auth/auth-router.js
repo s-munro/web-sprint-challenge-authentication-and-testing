89452b01702cd3814d498218d7728c83373b55fc
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-29 13:24:25 -0500

    all codegrade tests passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -17,7 +17,7 @@ router.post("/register", isValid, isUnique, (req, res) => {
 
   Users.add(credentials)
     .then((user) => {
-      res.status(201).json({ data: user });
+      res.status(201).json(user);
     })
     .catch((error) => {
       res.status(500).json({ message: error.message });
